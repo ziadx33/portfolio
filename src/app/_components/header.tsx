400d944d8443cb630bf0fa5efd4144fb9ef1366e
thegreatagen1 <hatemziad384@gmail.com>
2024-01-30 02:15:34 +0200

    fixed colors in header.tsx

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -22,7 +22,7 @@ export function Header() {
             <Button variant="outline" className="block md:hidden" onClick={toggleList}>
                 {listOpen ? <X /> : <Menu />}
             </Button>
-            <ul className={`items-center gap-4 font-semibold md:flex transition hidden [&.open]:flex [&.open]:bg-white [&.open]:fixed [&.open]:z-50 [&.open]:left-0 [&.open]:top-[85px] [&.open]:w-full [&.open]:h-[calc(100vh-85px)] [&.open]:justify-center [&.open]:flex-col ${listOpen ? "open" : ""}`}>
+            <ul className={`items-center gap-4 font-semibold md:flex transition hidden [&.open]:flex [&.open]:bg-white [&.open]:dark:bg-slate-950 [&.open]:fixed [&.open]:z-50 [&.open]:left-0 [&.open]:top-[85px] [&.open]:w-full [&.open]:h-[calc(100vh-85px)] [&.open]:justify-center [&.open]:flex-col ${listOpen ? "open" : ""}`}>
                 <li onClick={toggleList}>
                     <a className="transition hover:text-gray-600 dark:hover:text-slate-100 md:w-fit w-full md:h-fit h-24" href="#about">About me</a>
                 </li>
