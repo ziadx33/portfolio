76316f5b9cd4acb4e773f6564be71de4bdf91aa9
thegreatagen1 <hatemziad384@gmail.com>
2024-02-01 10:52:58 +0200

    changed the code

--- src/app/loading-page.tsx
+++ src/app/loading-page.tsx
@@ -3,17 +3,16 @@ import { Vim } from "@/components/logo";
 import { Progress } from "@/components/ui/progress";
 import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap"
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 type LoadingPageInterface = { loadings: { name: string; status: boolean }[] };
 
 export default function LoadingPage({ loadings }: LoadingPageInterface) {
     const totalTasks = loadings.length;
-    const [windowDimensions, setWindowDimensions] = useState<{ width: number; height: number }>({ width: 0, height: 0 });
+    const loadingContainer = useRef<HTMLDivElement>(null)
     useEffect(() => {
         document.body.style.overflow = "hidden"
         window.scrollTo(0, 0)
-        setWindowDimensions({ width: window.innerWidth, height: window.innerHeight })
     }, [])
 
     const overallProgress =
@@ -25,34 +24,36 @@ export default function LoadingPage({ loadings }: LoadingPageInterface) {
 
 
     useGSAP(async () => {
-        gsap.to("body :is(section, header)", {
-            y: window.innerHeight - 650,
-        })
-        if (overallProgress === 100) {
-            await new Promise((resolve) => setTimeout(resolve, 700))
-            const swipeDuration = 0.8
-            gsap.to(".loading-container", {
-                borderBottomLeftRadius: (windowDimensions.width / 2),
-                borderBottomRightRadius: (windowDimensions.width / 2),
-                y: -window.innerHeight,
-                duration: swipeDuration,
-                ease: "power1.inOut",
-                onComplete() {
-                    document.body.style.overflow = "auto"
-                    document.querySelector(".loading-container")?.remove()
-                }
-            })
+        if (loadingContainer.current) {
             gsap.to("body :is(section, header)", {
-                y: 0,
-                duration: swipeDuration,
-                ease: "power1.inOut",
+                y: loadingContainer.current?.clientHeight - 650,
             })
+            if (overallProgress === 100) {
+                await new Promise((resolve) => setTimeout(resolve, 700))
+                const swipeDuration = 0.8
+                gsap.to(".loading-container", {
+                    borderBottomLeftRadius: (loadingContainer.current.clientWidth / 2),
+                    borderBottomRightRadius: (loadingContainer.current.clientHeight / 2),
+                    y: -window.innerHeight,
+                    duration: swipeDuration,
+                    ease: "power1.inOut",
+                    onComplete() {
+                        document.body.style.overflow = "auto"
+                        document.querySelector(".loading-container")?.remove()
+                    }
+                })
+                gsap.to("body :is(section, header)", {
+                    y: 0,
+                    duration: swipeDuration,
+                    ease: "power1.inOut",
+                })
+            }
         }
     })
 
     return (
-        <div className="loading-container border-2 w-full h-full fixed bg-white z-50 dark:bg-slate-950 flex flex-col items-center justify-center px-8 md:px-12 lg:px-24 3xl:px-[30rem]">
-            <h1 className="md:text-6xl text-3xl font-bold w-fit mx-auto md:mb-12 mb-6 transition md:text-start text-center">welcome to TheGreatagen <Vim width={windowDimensions.width > 768 ? 90 : 50} height={90} /></h1>
+        <div ref={loadingContainer} className="loading-container border-2 w-full h-full fixed bg-white z-50 dark:bg-slate-950 flex flex-col items-center justify-center px-8 md:px-12 lg:px-24 3xl:px-[30rem]">
+            <h1 className="md:text-6xl text-3xl font-bold w-fit mx-auto md:mb-12 mb-6 transition md:text-start text-center">welcome to TheGreatagen <Vim width={90} height={90} /></h1>
             <h2 className="md:text-5xl text-2xl font-semibold md:mb-10 mb-6 transition">Loading...</h2>
             <Progress className="w-full md:h-4 h-3 mb-4 transition" value={rest} />
             <p className="transition">Overall Progress: {rest}%</p>
