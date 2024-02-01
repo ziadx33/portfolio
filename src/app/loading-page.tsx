e259a9335736dbf32d4802859ff228749ebca6bb
thegreatagen1 <hatemziad384@gmail.com>
2024-02-01 10:29:24 +0200

    fixed a simple error

--- src/app/loading-page.tsx
+++ src/app/loading-page.tsx
@@ -3,15 +3,17 @@ import { Vim } from "@/components/logo";
 import { Progress } from "@/components/ui/progress";
 import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 type LoadingPageInterface = { loadings: { name: string; status: boolean }[] };
 
 export default function LoadingPage({ loadings }: LoadingPageInterface) {
     const totalTasks = loadings.length;
+    const [windowDimensions, setWindowDimensions] = useState<{ width: number; height: number }>({ width: 0, height: 0 });
     useEffect(() => {
         document.body.style.overflow = "hidden"
         window.scrollTo(0, 0)
+        setWindowDimensions({ width: window.innerWidth, height: window.innerHeight })
     }, [])
 
     const overallProgress =
@@ -30,8 +32,8 @@ export default function LoadingPage({ loadings }: LoadingPageInterface) {
             await new Promise((resolve) => setTimeout(resolve, 700))
             const swipeDuration = 0.8
             gsap.to(".loading-container", {
-                borderBottomLeftRadius: (window.innerWidth / 2),
-                borderBottomRightRadius: (window.innerWidth / 2),
+                borderBottomLeftRadius: (windowDimensions.width / 2),
+                borderBottomRightRadius: (windowDimensions.width / 2),
                 y: -window.innerHeight,
                 duration: swipeDuration,
                 ease: "power1.inOut",
@@ -50,7 +52,7 @@ export default function LoadingPage({ loadings }: LoadingPageInterface) {
 
     return (
         <div className="loading-container border-2 w-full h-full fixed bg-white z-50 dark:bg-slate-950 flex flex-col items-center justify-center px-8 md:px-12 lg:px-24 3xl:px-[30rem]">
-            <h1 className="md:text-6xl text-3xl font-bold w-fit mx-auto md:mb-12 mb-6 transition md:text-start text-center">welcome to TheGreatagen <Vim width={window.innerWidth > 768 ? 90 : 50} height={90} /></h1>
+            <h1 className="md:text-6xl text-3xl font-bold w-fit mx-auto md:mb-12 mb-6 transition md:text-start text-center">welcome to TheGreatagen <Vim width={windowDimensions.width > 768 ? 90 : 50} height={90} /></h1>
             <h2 className="md:text-5xl text-2xl font-semibold md:mb-10 mb-6 transition">Loading...</h2>
             <Progress className="w-full md:h-4 h-3 mb-4 transition" value={rest} />
             <p className="transition">Overall Progress: {rest}%</p>
