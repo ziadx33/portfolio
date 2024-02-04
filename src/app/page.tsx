855466ee13a8181a5d0e8441eac41e2ee90ca08c
thegreatagen1 <hatemziad384@gmail.com>
2024-02-04 11:24:02 +0200

    fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useProjects, useStats } from "@/store";
 import { Header } from "./_components/header";
 import About from "./_sections/about/about";
@@ -12,38 +12,59 @@ import { Footer } from "./_components/footer";
 import axios from "axios";
 import { Button } from "@/components/ui/button";
 import { ArrowUp } from "lucide-react";
-import { useIntersectionObserver } from "usehooks-ts"
+import { useIntersectionObserver } from "usehooks-ts";
 
 export default function Page() {
-    const { isLoading: isProjectsLoading, setProjects } = useProjects(store => store)
-    const { isLoading: isStatsLoading, setStats } = useStats(store => store)
-    const aboutRef = useRef<HTMLDivElement>(null)
-    const aboutObserver = useIntersectionObserver(aboutRef, { threshold: 1 })
-    const isAboutVisable = !!aboutObserver?.isIntersecting
-    
-    useEffect(() => {
-        (async () => {
-            const projects = await getProjects()
-            setProjects(projects)
-            const promises = [
-                axios.get("https://api.github.com/users/thegreatagen1"),
-                axios.get("https://www.codewars.com/api/v1/users/thegreatagen"),
-            ]
-            const [{ data: github }, { data: codewars }] = await Promise.all(promises)
-            setStats({ github: github.public_repos, codewars: codewars.codeChallenges.totalCompleted })
-        })()
-    }, [setProjects, setStats])
+	const { isLoading: isProjectsLoading, setProjects } = useProjects(
+		(store) => store,
+	);
+	const { isLoading: isStatsLoading, setStats } = useStats((store) => store);
+	const aboutRef = useRef<HTMLDivElement>(null);
+	const aboutObserver = useIntersectionObserver(aboutRef, { threshold: 1 });
+	const isAboutVisable = !!aboutObserver?.isIntersecting;
 
-    return <>
-        <LoadingPage loadings={[{ name: "projects", status: isProjectsLoading }, { name: "stats", status: isStatsLoading }]} />
-        <Header />
-        <Hero />
-        <About>
-            <div ref={aboutRef} />
-        </About>
-        <Projects />
-        <Contact />
-        <Footer />
-        <Button size="icon" onClick={() => scrollTo({ top: 0, behavior: "smooth" })} className={`fixed bottom-12 right-16 z-30 transition-opacity opacity-0 ${(isAboutVisable || document.documentElement.scrollTop > 900) ? "opacity-100" : ""}`}><ArrowUp /></Button>
-    </>
+	useEffect(() => {
+		(async () => {
+			const projects = await getProjects();
+			setProjects(projects);
+			const promises = [
+				axios.get("https://api.github.com/users/thegreatagen1"),
+				axios.get(
+					"https://www.codewars.com/api/v1/users/thegreatagen",
+				),
+			];
+			const [{ data: github }, { data: codewars }] =
+				await Promise.all(promises);
+			setStats({
+				github: github.public_repos,
+				codewars: codewars.codeChallenges.totalCompleted,
+			});
+		})();
+	}, [setProjects, setStats]);
+
+	return (
+		<>
+			<LoadingPage
+				loadings={[
+					{ name: "projects", status: isProjectsLoading },
+					{ name: "stats", status: isStatsLoading },
+				]}
+			/>
+			<Header />
+			<Hero />
+			<About>
+				<div ref={aboutRef} />
+			</About>
+			<Projects />
+			<Contact />
+			<Footer />
+			<Button
+				size="icon"
+				onClick={() => scrollTo({ top: 0, behavior: "smooth" })}
+				className={`fixed bottom-12 right-16 z-30 transition-opacity opacity-0 ${isAboutVisable || document?.documentElement.scrollTop > 900 ? "opacity-100" : ""}`}
+			>
+				<ArrowUp />
+			</Button>
+		</>
+	);
 }
