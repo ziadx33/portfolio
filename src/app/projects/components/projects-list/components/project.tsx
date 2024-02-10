22877551311d4406bfbd93d5bee3c1100597a3a5
thegreatagen1 <hatemziad384@gmail.com>
2024-02-10 05:48:24 +0200

    performance change

--- src/app/projects/components/projects-list/components/project.tsx
+++ src/app/projects/components/projects-list/components/project.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Project } from "@/utils/types";
 import { Check, Code, Eye } from "lucide-react";
+import Image from "next/image";
 import { Dispatch, SetStateAction } from "react";
 
 export default function Project({ image, name, description, technologies, url, code, setTechnologiesList, completedInDays, viewAs }: Project & { setTechnologiesList: Dispatch<SetStateAction<TechnologiesList>>, completedInDays: number, viewAs: Sorts["viewAs"] }) {
@@ -16,14 +17,14 @@ export default function Project({ image, name, description, technologies, url, c
         <div className="flex gap-1 flex-wrap mb-2.5">
             {technologies.map(technology => {
                 return <Button onClick={() => setTechnologiesList(technologiesList => technologiesList.map(tech => { return { ...tech, checked: tech.name === technology } }))} className="h-9 w-fit border-2 rounded-xl dark:hover:bg-transparent" variant="outline" key={technology} title={technology}>
-                    <img draggable="false" className="h-full w-fit mr-2" src={`/logos/${technology.split(" ").join("-").toLowerCase()}.png`} />
+                    <img draggable="false" alt={technology} className="h-full w-fit mr-2" src={`/logos/${technology.split(" ").join("-").toLowerCase()}.png`} />
                     {technology}
                 </Button>
             })}
         </div>
     </>
-    return <Card className={`${isList ? "w-full flex pb-0" : "w-96 "} h-fit overflow-hidden shadow-xl`}>
-        <img src={image} alt={name} draggable='false' className={isList ? "w-96 border-r" : "w-full h-44"} />
+    return <Card className={`${isList ? "w-full flex pb-0" : "w-96 "} h-fit overflow-hidden shadow-xl relative`}>
+        <Image width={isList ? 2000 : 1920} height={1080} src={image} alt={name} draggable='false' className="object-cover" />
         <div className={`pt-2 px-4 ${isList ? "flex flex-col justify-between" : ""}`}>
             {isList ? <div>{heading}</div> : heading}
             <div className="flex gap-1 mt-auto pb-3">
