cc57f38fe34ed9017cc1f0beab2b58735ec2b628
ziadx33 <hatemziad384@gmail.com>
2024-07-18 00:21:51 +0300

    fix: fixed responsive issue

--- src/app/_components/stats/stats.tsx
+++ src/app/_components/stats/stats.tsx
@@ -1,13 +1,32 @@
-"use client"
+"use client";
 import { Code } from "lucide-react";
 import Stat from "./components/stat";
 import { useStats } from "@/store";
 
 export default function Stats() {
-    const { setStats: _, isLoading: __, ...stats } = useStats(store => store)
-    return <div className="mt-5 flex gap-2.5">
-        {stats && <>
-            {Object.keys(stats).map(key => <Stat key={key} name={key} value={stats[key as keyof typeof stats] as number} img={<Code size={40} />} />)}
-        </>}
-    </div>
+	const {
+		setStats: _,
+		isLoading: __,
+		...stats
+	} = useStats((store) => store);
+	return (
+		<div className="mt-5 lg:w-full items-center flex gap-2.5">
+			{stats && (
+				<>
+					{Object.keys(stats).map((key) => (
+						<Stat
+							key={key}
+							name={key}
+							value={
+								stats[
+									key as keyof typeof stats
+								] as number
+							}
+							img={<Code size={40} />}
+						/>
+					))}
+				</>
+			)}
+		</div>
+	);
 }
