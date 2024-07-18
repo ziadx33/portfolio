dc82569179b3b1775a437c134856c740c1c5a73e
ziadx33 <hatemziad384@gmail.com>
2024-07-18 17:40:00 +0300

    fix: fixed design error

--- src/app/_components/stats/stats.tsx
+++ src/app/_components/stats/stats.tsx
@@ -6,7 +6,7 @@ import { useStats } from "@/store";
 export default function Stats() {
 	const { setStats: _, isLoading: __, github } = useStats((store) => store);
 	return (
-		<div className="mt-5 lg:w-full items-center flex gap-2.5">
+		<div className="mt-5 max-lg:w-full justify-center flex gap-2.5">
 			{github && (
 				<Stat
 					key={github}
